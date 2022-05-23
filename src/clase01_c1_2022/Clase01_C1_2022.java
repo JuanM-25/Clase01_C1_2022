c1742976c29310e3cb2cbe0edff4ab05aa4ff441
JuanM-25 <jdmh25@gmail.com>
2022-05-22 20:25:34 -0600

    Mi primer commit.

---
+++ src/clase01_c1_2022/Clase01_C1_2022.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase01_c1_2022;
+
+/**
+ *
+ * @author Juan Diego
+ */
+public class Clase01_C1_2022 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      
+        // Variables
+        int a = 0;
+        int b = 1;
+        
+        // Estructura Decision
+        if(a==b){
+            System.out.println("Las variables son iguales");
+        }
+        
+        System.out.print("Hola mundo!");
+        System.out.println("Temporada de patos!");
+        System.out.println("Temporada de conejos!");
+        System.out.print("Esto es todo amigos!");
+    }
+    
+}
